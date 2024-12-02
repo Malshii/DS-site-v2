d27cc71419ce1e0d726cc99b4e1633a4bcd1b62e
Malshi Kulasinghe <kulasinghe1malshi@gmail.com>
2024-12-02 07:44:18 +0530

    add robot text and site map

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://gdcdigital.net/",
+      lastModified: new Date("2024-12-02T01:54:32+00:00"),
+      priority: 1.0,
+    },
+    {
+      url: "https://gdcdigital.net/about",
+      lastModified: new Date("2024-12-02T01:54:32+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://gdcdigital.net/contact-us",
+      lastModified: new Date("2024-12-02T01:54:32+00:00"),
+      priority: 0.8,
+    },
+    {
+      url: "https://gdcdigital.net/schedule-consultation",
+      lastModified: new Date("2024-12-02T01:54:32+00:00"),
+      priority: 0.8,
+    },
+  ];
+}
